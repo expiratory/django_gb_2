725b193a9100e0b9a9cd214ab1b7c796e6772934
Alexandr Kilchevskiy <alexkill94@mail.ru>
2024-02-24 23:09:31 +0300

    hw 5 added

--- app/admin.py
+++ app/admin.py
@@ -5,14 +5,21 @@ from .models import Client, Product, Order
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'email', 'phone_number',)
+    search_fields = ('name', 'email', 'phone_number',)
+    ordering = ('name',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'price', 'quantity',)
+    search_fields = ('name',)
+    ordering = ('name',)
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'client', 'total_amount', 'order_date',)
+    search_fields = ('client__name', 'order_date',)
+    raw_id_fields = ('client',)
+    ordering = ('order_date',)
